23066cb93b964d15acb7bc60422f7992087c6e70
Stephen Studer <stephenkstuder@gmail.com>
2023-09-24 10:49:50 -0700

    Change todos to tasks

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <h3>TODO LIST</h3>
+        <h3>TASKS</h3>
         <ul class="list-group">
             <TodoItem class="list-group-item px-2 py-3" v-for="(todo, i) in todos" :key="i" v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
         </ul>
     </div>
 </template>
 <script>
-import TodoItem from "./TodoItem.vue";
+import TodoItem from "./TodoItem";
 export default {
     name: "Todos",
     components: {
